fe96d5aee6d189d8c1b5d9747a9b4fa796592ac4
laanatri <laanatri.ade@gmail.com>
2025-02-28 12:33:35 +0100

    localstorage

--- script.js
+++ script.js
@@ -2,30 +2,60 @@ const form = document.querySelector("form");
 const quoteInput = document.querySelector("#quote-input");
 const authorInput = document.querySelector("#author-input");
 const quoteList = document.querySelector("#quote-list");
+let quoteCount  = 0;
+let quotesArray = [];
 
-const addQuote = (quote, author) => {
+const updateCount = () => {
+    const count = document.getElementById("count");
+    quoteCount += 1;
+    count.innerText = `${quoteCount} ${quoteCount <= 1 ? "citation" : "citations"}`;
+}
+
+const displayQuote = (q, a) => {
     const paragrapheText = document.createElement("p");
-    paragrapheText.classList.add("author");
+    paragrapheText.classList.add("text");
     const paragrapheAuthor = document.createElement("p");
     paragrapheAuthor.classList.add("author");
-
-    paragrapheText.innerText = quote;
-    paragrapheAuthor.innerText = author;
-
+    paragrapheText.innerText = q;
+    paragrapheAuthor.innerText = a;
     const quoteContent = document.createElement("div");
     quoteContent.classList.add("quote");
     quoteContent.append(paragrapheText, paragrapheAuthor);
     quoteList.appendChild(quoteContent);
 }
 
-form.addEventListener("submit", () => {
-    event.preventDefault();
+const addQuote = (quote, author) => {
+    displayQuote(quote, author);
+    updateCount();
+    const citation = {
+        author: author,
+        text: quote
+    };
+    quotesArray.push(citation);
+    localStorage.setItem("citations", JSON.stringify(quotesArray));
+}
 
+const displayList = (list) => {
+    list.forEach((c, i) => {
+        displayQuote(list[i].text, list[i].author);
+    })
+}
+
+window.addEventListener("load", () => {
+    const listCitation = JSON.parse(localStorage.getItem("citations"));
+    if (listCitation) {
+        listCitation.forEach(c => {
+            quotesArray.push(c);
+            updateCount();
+        })
+        displayList(listCitation);
+    }
+})
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
     let text = quoteInput.value;
     let author = authorInput.value;
-
-    console.log(text)
-    console.log(author)
-
-    addQuote(text, author)
+    addQuote(text, author);
+    e.target.reset();
 })
