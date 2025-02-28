8e7c3171163618300f82f567a4df6552a0253c7c
laanatri <laanatri.ade@gmail.com>
2025-02-28 10:46:11 +0100

    add quotes

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 const form = document.querySelector("form");
 const quoteInput = document.querySelector("#quote-input");
 const authorInput = document.querySelector("#author-input");
-let text = "";
-let author = "";
+const quoteList = document.querySelector("#quote-list");
+
+const addQuote = (quote, author) => {
+    const paragrapheText = document.createElement("p");
+    paragrapheText.classList.add("author");
+    const paragrapheAuthor = document.createElement("p");
+    paragrapheAuthor.classList.add("author");
+
+    paragrapheText.innerText = quote;
+    paragrapheAuthor.innerText = author;
+
+    const quoteContent = document.createElement("div");
+    quoteContent.classList.add("quote");
+    quoteContent.append(paragrapheText, paragrapheAuthor);
+    quoteList.appendChild(quoteContent);
+}
 
 form.addEventListener("submit", () => {
     event.preventDefault();
 
-    text = quoteInput.value;
-    author = authorInput.value;
+    let text = quoteInput.value;
+    let author = authorInput.value;
 
     console.log(text)
     console.log(author)
+
+    addQuote(text, author)
 })
